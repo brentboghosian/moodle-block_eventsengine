1ec303486abc3d76b854ef52d7f73ef894fb6a9a
Brent Boghosian <brent.boghosian@remote-learner.net>
2017-05-08 13:55:28 -0400

    Version file.

---
+++ version.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Block EventsEngine block_eventsengine version file.
+ *
+ * @package   block_eventsengine
+ * @category  event
+ * @copyright 2017 onwards Brent Boghosian <brentboghosian@alumni.uwaterloo.ca>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2017050700;
+$plugin->requires = 2016052300;
+$plugin->component = 'block_eventsengine';
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '3.1.0.0';
