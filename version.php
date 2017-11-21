dca9b796a827d498924fbef3c5f23fa02e7f8808
Brent Boghosian <brent.boghosian@remote-learner.net>
2017-11-21 15:35:58 -0500

    Version 3.2.0.0

--- version.php
+++ version.php
@@ -10,8 +10,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017050700;
-$plugin->requires = 2016052300;
+$plugin->version = 2017051100;
+$plugin->requires = 2016120500;
 $plugin->component = 'block_eventsengine';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1.0.0';
+$plugin->release = '3.2.0.0';
